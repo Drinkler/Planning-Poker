1dec92ba3162e1f6008771d5ce6350628cb9cb3b
Drinkler <flo.drinkler@gmail.com>
2020-04-03 16:29:56 +0200

    checks if email already exists

--- src/php/database.php
+++ src/php/database.php
@@ -1,6 +1,6 @@
 <?php
-$link = new mysqli($_SERVER['RDS_HOSTNAME'], $_SERVER['RDS_USERNAME'], $_SERVER['RDS_PASSWORD'], $_SERVER['RDS_DB_NAME'], $_SERVER['RDS_PORT']);
+$mysqli = new mysqli($_SERVER['RDS_HOSTNAME'], $_SERVER['RDS_USERNAME'], $_SERVER['RDS_PASSWORD'], $_SERVER['RDS_DB_NAME'], $_SERVER['RDS_PORT']);
 
-if (!$link) {
+if (!$mysqli) {
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
---
+++ src/php/signup.php
@@ -0,0 +1,29 @@
+<?php
+
+require("database.php");
+
+// Get required data
+if (!empty($_POST["name"]) && !empty($_POST["surname"]) && !empty($_POST["email"]) && !empty($_POST["password"])) {
+    $name = $mysqli->real_escape_string(htmlspecialchars($_POST["name"]));
+    $surname = $mysqli->real_escape_string(htmlspecialchars($_POST["surname"]));
+    $email = $mysqli->real_escape_string(htmlspecialchars($_POST["email"]));
+    $password = password_hash($mysqli->real_escape_string(htmlspecialchars($_POST["password"])), PASSWORD_DEFAULT);
+}
+
+// TODO: Check if email already exists
+$query = "SELECT COUNT(*) FROM user WHERE email=?";
+$stmt = $mysqli->prepare($query);
+$stmt->bind_param("s", $email);
+$stmt->execute();
+$result = $stmt->get_result();
+$stmt->close();
+
+if (!$result->fetch_array()[0]) {
+    $query = "INSERT INTO user (name ,surname ,email ,password) VALUES (?,?,?,?)";
+    $stmt = $mysqli->prepare($query);
+    $stmt->bind_param("ssss", $name, $surname, $email, $password);
+    $stmt->execute();
+    $stmt->close();
+} else {
+    echo "Email existiert bereits.";
+}
