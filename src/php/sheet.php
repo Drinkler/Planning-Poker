56a8d7067a0ac974b2c9aaf985ae4ee4168637ab
Drinkler <flo.drinkler@gmail.com>
2020-04-16 09:49:13 +0200

    added querys

--- src/php/sheet.php
+++ src/php/sheet.php
@@ -5,3 +5,9 @@
 // creator: fk iduser (int)
 // deck: int
 $query = "INSERT INTO lobby (name, creator, deck) VALUES (:name, :creator, :deck)";
+
+// Get all lobbys from a creator
+$query = "SELECT * FROM lobby WHERE creator = :iduser";
+
+// Delete a lobby
+$query = "DELETE FROM lobby WHERE idlobby = :idlobby";
