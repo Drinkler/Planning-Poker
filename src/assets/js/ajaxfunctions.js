d1915a7c1616c83243650179c5a3cd5a5742dac7
Luca Stanger <lstanger@gmx.de>
2020-05-23 00:01:32 +0200

    refactored

--- src/assets/js/ajaxfunctions.js
+++ src/assets/js/ajaxfunctions.js
@@ -1,11 +1,15 @@
+function activateStory() {
+    alert("Test");
+}
+
 function fetchStoriesFromGitHub() {
     let navContent = document.getElementById("nav-github");
     let url = document.getElementById("githubInput").value;
     let username = url.split("/")[3];
     let repository = url.split("/")[4];
 
-    let curlURL = `https://api.github.com/repos/${username}/${repository}/issues`;
 
+    let curlURL = `https://api.github.com/repos/${username}/${repository}/issues`;
     let storyTable = document.createElement("table");
     storyTable.className = "table";
     let thead = document.createElement("thead");
@@ -15,8 +19,13 @@ function fetchStoriesFromGitHub() {
     let thDescription = document.createElement("th");
     thDescription.setAttribute("scope", "col");
     thDescription.innerHTML = "Description";
+    let thButton = document.createElement("th");
+    thButton.setAttribute("scope", "col");
+    thButton.innerHTML = "";
+    thButton.addEventListener("click", function () {activateStory}, false);
     thead.appendChild(thTitle);
     thead.appendChild(thDescription);
+    thead.appendChild(thButton);
     storyTable.appendChild(thead);
     let tBody = document.createElement("tbody");
 
@@ -29,10 +38,14 @@ function fetchStoriesFromGitHub() {
                     let tr = document.createElement("tr");
                     let tdTitle = document.createElement("td");
                     let tdDescription = document.createElement("td");
+                    let activationButton = document.createElement("button");
                     tdTitle.innerHTML = element["title"];
                     tdDescription.innerHTML = element["body"];
+                    activationButton.innerHTML = "Set Active";
+                    activationButton.className = "btn btn-outline-primary activate";
                     tr.appendChild(tdTitle);
                     tr.appendChild(tdDescription);
+                    tr.appendChild(activationButton);
                     tBody.appendChild(tr);
                 },
                 error: function () {
